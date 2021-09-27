3edfed6c0f5137cc3ccad57f52a2a1ee3392657a
Kiruse <26940742+Kiruse@users.noreply.github.com>
2021-09-27 14:45:45 +0200

    added dimensions helpers

--- dist/domops.esm.d.ts
+++ dist/domops.esm.d.ts
@@ -57,9 +57,50 @@ export declare class DocQuery {
     html(): string[];
     html(value: string): DocQuery;
     html(cb: (el: HTMLElement, i: number, ary: HTMLElement[]) => string): DocQuery;
+    value(): string[];
+    value(val: string): DocQuery;
+    value(cb: (el: HTMLElement, i: number, ary: HTMLElement[]) => string): DocQuery;
     style(name: string): string[];
     style(name: string, value: any): DocQuery;
     style(styles: Object): DocQuery;
+    /**
+     * Get the absolute location on the page of each element in the current selection.
+     * @returns Array of 2-tuples [[x, y]]
+     */
+    location(): [number, number][];
+    private _elementLocation;
+    /**
+     * Get the absolute size of each element in the current selection.
+     * @returns Array of 2-tuples [[x, y]]
+     */
+    size(): [number, number][];
+    /**
+     * Get the offsets of each element relative to their individual offset parents.
+     * The offset parent is the first ancestor element with CSS display set to anything but 'static'. In other words,
+     * it's the element considered as the anchor point by CSS.
+     *
+     * `right` and `bottom` are regular distances, i.e. distance from anchor parent's left border to element's right
+     * border respectively.
+     *
+     * @returns Array of 4-tuples [[left, top, right, bottom]]
+     */
+    box({ absolute }: {
+        absolute: boolean;
+    }): [number, number, number, number][];
+    /**
+     * Get the offsets of each element relative to their individual offset parents as CSS-compatible objects array.
+     *
+     * Unlike {@see box}, `right` and `bottom` properties are reversed distances, i.e. distance from anchor parent's
+     * right border to element's own right border respectively.
+     *
+     * @returns Array of CSS location objects [{top, left, right, bottom}]
+     */
+    cssBox(): {
+        top: number;
+        left: number;
+        right: number;
+        bottom: number;
+    }[];
     private _style_get;
     private _style_set;
     private _style_obj;
@@ -68,6 +109,12 @@ export declare class DocQuery {
     hasClass(cls: string): boolean[];
     on(evt: string, handler: DOMEventHandler): DocQuery;
     forEach(cb: (el: HTMLElement, idx: number, ary: HTMLElement[]) => void): DocQuery;
+    /**
+     * Transform each element in the current selection and return the new selection.
+     * @param cb transformer to apply
+     * @returns new selection of transformed elements
+     */
+    map(cb: (el: HTMLElement, idx: number, ary: HTMLElement[]) => HTMLElement): DocQuery;
     /**
      * Attach every element in the current selection to the given parent element.
      * @param parent parent element to attach to
@@ -81,7 +128,7 @@ export declare class DocQuery {
      * @param children to attach
      * @return this selection
      */
-    attach(...children: HTMLElement[]): DocQuery;
+    attach(...children: (HTMLElement | string)[]): DocQuery;
     /**
      * Detach the current selection from their parent elements.
      * @note The elements in this selection will be effectively removed from the DOM until reattached.
@@ -89,6 +136,12 @@ export declare class DocQuery {
      * @returns this selection
      */
     detach(): DocQuery;
+    /**
+     * Gets the parent element of every element in the current selection.
+     * If multiple elements share a parent, that parent is added only once.
+     * @returns new selection of parent elements
+     */
+    parent(): DocQuery;
     /**
      * Detach all children from the elements of the current selection.
      * @returns this selection
